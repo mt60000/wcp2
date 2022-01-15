af6ca5bdd5d3c29f834a7d017741e99c31c6f255
mt60000 <niruyaku45@gmail.com>
2022-01-15 10:29:56 +0900

    practice

---
+++ Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "mochimo";
+		System.out.println(name);
+		name = "tyo";
+		System.out.println(name);
+		final int number = 6;
+		System.out.println(number);
+	}
+}
---
+++ Chapter05.java
@@ -0,0 +1,19 @@
+
+public class Chapter05 {
+	public static void main(String[]args) {
+		long num = 38279L;
+		double num2 = 3.15;
+		boolean boo = true;
+		System.out.println(num);
+		System.out.println(num2);
+		System.out.println(boo);
+		Float num3 = 6.66F;
+		Integer num4 = 666;
+		Character char1 = '寅';
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(char1);
+		int[] nums = { 16, 24, 87, 66, 44 };
+		System.out.println(nums[3]);
+	}
+}
